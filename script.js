4dd0b45f6b113773b62c315ee255cd71badc4e27
Andrea Hayward <andrea.teofilo@yahoo.com>
2020-09-11 17:18:23 -0400

    starting js

---
+++ script.js
@@ -0,0 +1,4 @@
+var apiKey = "42d9910c0bcc018a50ba4f26eb2e6e41";
+var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?";
+var fiveDayWeatherURL = "https://api.openweathermap.org/data/2.5/weather?";
+var weatherUV = "https://api.openweathermap.org/data/2.5/weather?";
